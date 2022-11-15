217a3b79e2dc8032f1c5a3704afbd89fc0c22cb4
charlesK <115595915+charlesknapp@users.noreply.github.com>
2022-11-15 15:33:29 -0700

    Added next files

--- server.js
+++ server.js
@@ -2,34 +2,62 @@
 const express = require('express');
 const app = express();
 
+const fruits = require('./models/fruits');
+
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
 
 // ====== DB Connection ====== //
 
 
 
 // ====== Middleware location ====== //
-const fruits = ['apple', 'banana', 'pear'];
+app.use((req, res, next) => {
+    console.log('I run for all routes');
+    next();
+});
 
+app.use(express.urlencoded({extended:false}));
 
 // ====== Routes ====== //
 // I.N.D.U.C.E.S (Index, New, Delete, Update, Create, Edit, Show)
 
 // Index
 app.get('/fruits', (req, res) => {
-    res.send(fruits);
+    res.render('Index', {fruits: fruits });
 });
 
 // New
+app.get('/fruits/new', (req, res) => {
+    res.render('New');
+});
 
 // Delete
 
 // Update
 
 // Create
+app.post('/fruits', (req, res)=>{
+
+    if(req.body.readyToEat === 'on'){ //if checked, req.body.readyToEat is set to 'on'
+        req.body.readyToEat = true; //do some data correction
+    } else { //if not checked, req.body.readyToEat is undefined
+        req.body.readyToEat = false; //do some data correction
+    }
+    
+    fruits.push(req.body);
+    console.log(fruits);
+    res.redirect('/fruits');
+});
 
 // Edit
 
 // Show
+app.get("/fruits/:index", (req, res) => {
+    res.render('Show', { //second param must be an object
+        fruit: fruits[req.params.index] //there will be a variable available inside the ejs file called fruit, its value is fruits[req.params.index]
+    });
+})
 
 // ====== Listening for port ====== //
 app.listen(3000, () => {
